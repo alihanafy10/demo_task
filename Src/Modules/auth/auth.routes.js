a6bbb8860249ca7d155d01b3f1b3770a256956f4
alihanafy10 <engali1030@gmail.com>
2024-11-14 12:29:13 +0200

    3 commit

--- Src/Modules/auth/auth.routes.js
+++ Src/Modules/auth/auth.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { logout, profile, signin, signup } from "./auth.controller.js";
+import { logout, profile, refreshToken, signin, signup } from "./auth.controller.js";
 import { auth, validationMiddleware } from "../../Middleware/index.js";
 import { profileAuthSchema, signinAuthSchema, signupAuthSchema } from "./auth.validation.js";
 
@@ -10,3 +10,4 @@ authRouter.post('/signup',validationMiddleware(signupAuthSchema), signup)
 authRouter.post('/signin',validationMiddleware(signinAuthSchema), signin)
 authRouter.get('/profile',validationMiddleware(profileAuthSchema),auth, profile)
 authRouter.post('/logout',validationMiddleware(profileAuthSchema),auth, logout)
+authRouter.get('/refreshToken',validationMiddleware(profileAuthSchema), refreshToken)
